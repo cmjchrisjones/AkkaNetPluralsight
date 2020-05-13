1b5e9ed3671784db162e0953b0e44b5262ca5e54
Chris Jones <cmj.chris.jones@googlemail.com>
2020-05-13 13:57:20 +0100

    Simple MovieStreamingActorSystem from Module 1

---
+++ MovieStreamingActorSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreamingActorSystem
+{
+    public class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
