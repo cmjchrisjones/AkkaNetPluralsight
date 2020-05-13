95d4120c3d17ec2a186939d9340756fa4e54012c
Chris Jones <cmj.chris.jones@googlemail.com>
2020-05-13 17:19:23 +0100

    Renamed some actor refs which had props in the name

--- MovieStreamingActorSystem/Program.cs
+++ MovieStreamingActorSystem/Program.cs
@@ -17,26 +17,26 @@ namespace MovieStreamingActorSystem
             // Untyped Actor sending primitive objects
 
             var playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectProps = Props.Create<PlaybackActorThatInheritsUntypedActorAndHandlesPrimitiveObjects>();
-            var playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectPropsRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectProps, "playbackActorThatHandlesPrimitiveObjectProps");
+            var playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectProps, "playbackActorThatHandlesPrimitiveObjectProps");
 
             // Send Message
-            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectPropsRef.Tell("AKKA.NET: The Movie"); // This sends a string to the actor which is handled
-            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectPropsRef.Tell(42); // This sends an int to the actor which is handled
-            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectPropsRef.Tell('c'); // This sends a char to the actors which is NOT handled, but won't throw an exception
+            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectRef.Tell("AKKA.NET: The Movie"); // This sends a string to the actor which is handled
+            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectRef.Tell(42); // This sends an int to the actor which is handled
+            playbackActorThatInheritsUntypedActorAndHandlesPrimitiveObjectRef.Tell('c'); // This sends a char to the actors which is NOT handled, but won't throw an exception
 
             // Untyped Actor but using the message class
             var playbackActorThatInheritsUntypedActorAndHandlesMessagesProps = Props.Create<PlaybackActorThatInheritsUntypedActorAndHandlesMessages>();
-            var playbackActorThatInheritsUntypedActorAndHandlesMessagesPropsRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsUntypedActorAndHandlesMessagesProps, "playbackActorThatInheritsUntypedActorAndHandlesMessagesProps");
+            var playbackActorThatInheritsUntypedActorAndHandlesMessagesRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsUntypedActorAndHandlesMessagesProps, "playbackActorThatInheritsUntypedActorAndHandlesMessagesProps");
 
             // Send Message
             var message = new PlayMovieMessage("AKKA.NET: The Movie", 42);
-            playbackActorThatInheritsUntypedActorAndHandlesMessagesPropsRef.Tell(message);
+            playbackActorThatInheritsUntypedActorAndHandlesMessagesRef.Tell(message);
 
             // Receive Actor that handles the message class
             var playbackActorThatInheritsReceiveActorAndHandlesMessagesProps = Props.Create<PlaybackActorThatInheritsReceiveActorAndHandlesMessages>();
-            var playbackActorThatInheritsReceiveActorAndHandlesMessagesPropsRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsReceiveActorAndHandlesMessagesProps, "playbackActorThatInheritsReceiveActorAndHandlesMessagesProps");
+            var playbackActorThatInheritsReceiveActorAndHandlesMessagesRef = MovieStreamingActorSystem.ActorOf(playbackActorThatInheritsReceiveActorAndHandlesMessagesProps, "playbackActorThatInheritsReceiveActorAndHandlesMessagesProps");
 
-            playbackActorThatInheritsReceiveActorAndHandlesMessagesPropsRef.Tell(message);
+            playbackActorThatInheritsReceiveActorAndHandlesMessagesRef.Tell(message);
 
             Console.ReadLine();
             MovieStreamingActorSystem.Terminate();
