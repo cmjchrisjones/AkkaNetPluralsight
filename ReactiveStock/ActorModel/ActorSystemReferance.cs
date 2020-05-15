c6dc1a2af99e828bdce802e419ea0a9911e8c4e1
Chris Jones <cmj.chris.jones@googlemail.com>
2020-05-15 10:27:22 +0100

    Initial Setup from Introduction

---
+++ ReactiveStock/ActorModel/ActorSystemReferance.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.Ninject;
+using Ninject;
+using System;
+
+namespace ReactiveStock.ActorModel
+{
+    static class ActorSystemReferance
+    {
+        public static ActorSystem ActorSystem { get; private set; }
+
+        static ActorSystemReferance()
+        {
+            CreateActorSystem();
+        }
+
+        private static void CreateActorSystem()
+        {
+            ActorSystem = ActorSystem.Create("ReactiveStockActorSystem");
+            var container = new StandardKernel();
+
+            IDependencyResolver resolver = new NinjectDependencyResolver(container, ActorSystem);
+        }
+    }
+}
